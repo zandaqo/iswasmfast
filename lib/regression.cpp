ec8e51b7b4e2c481fb997869dc411b1644e417d3
Maga D. Zandaqo <denelxan@gmail.com>
2017-08-24 15:32:31 +0300

    refactor: use std methods for linear regression

--- lib/regression.cpp
+++ lib/regression.cpp
@@ -1,26 +1,19 @@
 #include <vector>
+#include <numeric>
 #include <math.h>
 
 std::vector<double> regression(std::vector<double> y, std::vector<double> x) {
   const size_t n = y.size();
-  double sumX = 0;
-  double sumY = 0;
-  double sumXy = 0;
-  double sumXx = 0;
-  double sumYy = 0;
-
-  for (size_t i = 0; i < n; i++) {
-    sumX += x[i];
-    sumY += y[i];
-    sumXy += (x[i] * y[i]);
-    sumXx += (x[i] * x[i]);
-    sumYy += (y[i] * y[i]);
-  }
+  double sumX = std::accumulate(x.begin(), x.end(), 0.0);
+  double sumY = std::accumulate(y.begin(), y.end(), 0.0);
+  double sumXy = std::inner_product(x.begin(), x.end(), y.begin(), 0.0);
+  double sumXx = std::inner_product(x.begin(), x.end(), x.begin(), 0.0);
+  double sumYy = std::inner_product(y.begin(), y.end(), y.begin(), 0.0);
 
   const double slope = (n * sumXy - sumX * sumY) / (n * sumXx - sumX * sumX);
   const double intercept = (sumY - slope * sumX) / n;
   const double r2 = pow((n * sumXy - sumX * sumY) /
     sqrt((n * sumXx - sumX * sumX) * (n * sumYy - sumY * sumY)), 2);
 
-  return std::vector<double>{ slope, intercept, r2 }
+  return std::vector<double>{ slope, intercept, r2 };
 }
